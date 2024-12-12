2ab549d9104ff5b34a318695f5acd15cff213ed4
kcwong395 <kcwong395@gmail.com>
2024-12-12 00:03:09 -0500

    fix function uri

--- modules/functions/main.tf
+++ modules/functions/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "function" {
-  image_uri       = "${var.registry}/${var.service_name}-${var.environment}:latest"
+  image_uri       = "public.ecr.aws/w3e5l9o0/user-service-int:latest"
   function_name   = "${var.service_name}-${var.environment}"
   role = var.role_arn
   package_type    = "Image"
