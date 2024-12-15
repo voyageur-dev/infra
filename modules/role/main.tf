81a327ab66cd6064a18c2ad27c35950d49882b17
kcwong395 <kcwong395@gmail.com>
2024-12-15 14:54:55 -0500

    add permission for role

--- modules/role/main.tf
+++ modules/role/main.tf
@@ -25,3 +25,27 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_role_policy" "lambda_ecr_policy" {
+  name = "${var.service_name}-${var.environment}-ecr-policy"
+  role = aws_iam_role.lambda_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr-public:GetAuthorizationToken",
+          "ecr-public:BatchCheckLayerAvailability",
+          "ecr-public:GetRepositoryPolicy",
+          "ecr-public:DescribeRepositories",
+          "ecr-public:DescribeImages",
+          "ecr-public:GetRepositoryCatalogData",
+          "ecr-public:GetRegistryCatalogData"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
