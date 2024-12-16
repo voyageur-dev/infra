4938b30da9286df811d3df57670b2933c54c51ba
kcwong395 <kcwong395@gmail.com>
2024-12-15 22:01:31 -0500

    create api gateway

--- modules/integration/main.tf
+++ modules/integration/main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id           = var.api_gateway_id
-  integration_type = "HTTP_PROXY"
+  integration_type = "AWS_PROXY"
   integration_method = "ANY"
   integration_uri    = var.integration_uri
 }
