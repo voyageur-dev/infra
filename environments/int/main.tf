52c3a8d75ac66ff63120b87fde6faa9d18d00244
kcwong395 <kcwong395@gmail.com>
2025-04-23 01:49:52 -0400

    add authorizer

--- environments/int/main.tf
+++ environments/int/main.tf
@@ -267,7 +267,7 @@ module "api_gateway" {
 
     # rb-service
     "GET /rb/questions" = {
-      authorizer_key = "cognito"
+      authorizer_key = "cognito-authorizer"
       integration = {
         uri                    = module.rb_service.lambda_function_invoke_arn
         payload_format_version = "2.0"
